959fc92efec824beaa93544dd334683c441bb232
LuisAngel1914 <i1217924@continental.edu.pe>
2026-08-31 14:31:21 -0500

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://miapp.alwaysdata.net/api',
+    baseURL: 'https://botica-backend-production.up.railway.app/api',
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
