6840d8911d626a882cd2690aff6f5c7ffe650ad1
LuisAngel1914 <i1217924@continental.edu.pe>
2026-09-09 11:31:10 -0500

    refactor(frontend): modernize src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
 <template>
-  <router-view></router-view>
+  <RouterView v-if="route.meta.public" />
+  <AppShell v-else>
+    <RouterView />
+  </AppShell>
 </template>
+
+<script setup>
+import { useRoute, RouterView } from 'vue-router';
+import AppShell from './components/AppShell.vue';
+
+const route = useRoute();
+</script>
